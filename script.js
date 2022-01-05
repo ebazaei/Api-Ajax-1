6241cceb6106e290da4486db43d67afcd37ec9e4
ehsan bazaei <bazaeiehsan@gmail.com>
2022-01-05 14:37:44 +0100

    test

--- script.js
+++ script.js
@@ -8,20 +8,25 @@ $(document).ready(function(){
         }
     };
     http.send()
+    //console.log(http)
 })
+
 
 
 function print(arr){
     arr.forEach( actor => {
-        const newTag = $('<div class="actor"></div>')
-        const actorName = $(`<h2>${actor.fullName}</h2>`)
-        const actorImage = $(`<img class="actor-image" src=${actor.imageUrl} alt=${actor.firstName} >`)
-        const actorTitle = $(`<p class="title">${actor.title}</p>`)
+        const newTag = $('<div class="actor"></div>');
+        const actorName = $(`<h2>${actor.fullName}</h2>`);
+        const actorImage = $(`<img class="actor-image" src=${actor.imageUrl} alt=${actor.firstName} >`);
+        const actorTitle = $(`<p class="title">${actor.title}</p>`);
+        const actorfamily = $(`<h2>${actor.id} - Family: ${actor.family}</h2>`);
 
-        newTag.append(actorImage)
         newTag.append(actorName)
         newTag.append(actorTitle)
+        newTag.append(actorImage)
+        newTag.append(actorfamily)
         $('#result').append(newTag)
+
     });
 }
 
